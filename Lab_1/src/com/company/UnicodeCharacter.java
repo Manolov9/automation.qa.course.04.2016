144b079c64deea4b8919863770411fee6ce5003a
Ceco <tsvetan.karchev@xoomworks.com>
2016-04-09 11:40:48 +0300

    delete some files

--- Lab_1/src/com/company/UnicodeCharacter.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class UnicodeCharacter {
-    public static void main(String[] args) {
-        char firstChar = '\u002A';
-
-        System.out.println(firstChar);
-    }
-}
--- Lab_1_SvetlanaDanailova/src/com/company/Hometown.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Hometown {
-    public static void main(String[] args) {
-
-        System.out.println("Vidin");
-    }
-
-}
