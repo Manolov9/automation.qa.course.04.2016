fa2d6d6a26123783f65208e0b0fc9efaa51c9b81
Luchezar Manolov <Luchezar Manolov>
2016-04-09 12:10:14 +0300

    LM COMMIT

---
+++ Lachezar Manolov/Projects_file/Struct_Java_DataEmployee/src/Holder.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Lucho on 09/04/2016.
+ */
+public class Holder {
+    public static void main(String[] args) {
+        String LastName = "Manolov";
+        String FirstName= "Lachezar";
+        String Name = LastName + FirstName;
+        int Age = 20;
+        String Sex = "Male";
+        double ID = 1388923452;
+        double Unique_Employee_ID = 222091002;
+
+
+        System.out.println("Name Holder Data is : " + Name );
+        System.out.println("His Age: " + Age );
+        System.out.println("Gender : " + Sex);
+        System.out.println("With special IDs: " + ID + Unique_Employee_ID);
+
+    }
+}
