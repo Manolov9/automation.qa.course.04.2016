feebffafcd1ce8854bd590f520d0bbad47d3e28e
alexmarinov69 <alex.marinov@netreact.com>
2016-04-09 12:33:02 +0300

    Nekvo doma6no

---
+++ Alex/Main5.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+ class ForDemo {
+    public static void main (String[]args) {
+
+
+
+
+        for (int c = 32;c< 128; c++) {
+            System.out.println(c + ": " + (char) c);
+        }
+
+        for (char letter = ' ';letter< 274; letter++) {
+            System.out.print(letter);
+        }
+
+    }
+}
