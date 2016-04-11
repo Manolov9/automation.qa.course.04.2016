4ca9fe88992c1a5629fd839cbb1a968d2bfbdabe
boyanivanov <ex_Xplisit@ab.bg>
2016-04-11 09:08:43 +0300

    my first commit

---
+++ Boyan Ivanov/lab1 AQ/src/com/company/Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int m = 32456;
+        long a = 0,u = 0,i = 4762762;
+        float p = 645735253,o = 999, t = 632626;
+        boolean one  = true, two = false;
+        char A = '\u0065';
+        String str = "test";
+        System.out.println('a');
+        // Homework1
+        // Print your hometown
+
+        System.out.println("Sofia");
+        //Current Date and Time
+
+        System.out.println("7.04.2016-12:40");
+        // Sum two integer number
+        int x = 4,z,y = 2;
+        int sum  = x + y;
+       System.out.println(sum);
+        // Declare Variables
+        byte w= -115;
+        byte e = 97;
+        short n = -10000;
+        int r = 52130;
+        long h = 4825932L ;
+        // Float or Double
+        float aa  = 34.567839023f;
+        float bb  = 12.345f;
+        float cc  = 8923.1234857f;
+        float dd = 3456.091f;
+        System.out.println(+ aa);
+        System.out.println(+ bb);
+        System.out.println(+ cc);
+        System.out.println(+ dd);
+        //Variable in Hexadecimal Format
+        String hex = "FE";
+        int value = Integer.parseInt(hex,16);
+        System.out.println(value);
+
+        // Boolean Variable
+         boolean isFemale = false;
+        System.out.println(isFemale);
+        // Strings and Objects
+
+        String First = "Hello";
+        String Second = "World";
+        System.out.println(First);
+        Object FirtsandSecond = "HelloWorld";
+
+
+        //Unicode Character//
+        String hexx = "2A";
+        char valuee = '\u002A';
+        System.out.println(valuee);
+
+        //Employee Data
+
+        String FirstName = "Boyan";
+        String LastName = "Ivanov";
+        byte age = 29;
+        char Gender = 'M';
+        long ID = 86000000l;
+        System.out.println(FirstName + LastName);
+        System.out.println(age+ID);
+        System.out.println(Gender);
+
+
+
+    }
+}
