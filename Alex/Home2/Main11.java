1e0125cc930ca8a06366399c70891319d560ab0c
alexmarinov69 <alex.marinov@netreact.com>
2016-04-19 21:57:15 +0300

    LIVE AGAIN

---
+++ Alex/Home2/Main11.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] numbers = {1,2,3,4,5,6,7,8,9,10,15,88,47,23,69,100,789,551,325};
+        for (int i = 0;i<20;i++) {
+        int num = (int) (Math.random()*20);
+
+
+
+
+
+            System.out.print(" " + (numbers[num]));
+
+
+        }
+
+
+
+	// write your code here
+    }
+}
---
+++ Alex/Lecture5 class/CatTests.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by Vesela on 19.4.2016 г..
+ */
+public class CatTests {
+
+    public static void main(String[] args) {
+
+        Cat malko_kote = new Cat("kiko","koki");
+        System.out.println(malko_kote.getName()+ " "+ malko_kote.getOwner());
+        System.out.println(malko_kote.name);
+
+
+
+
+
+
+
+
+    }
+}
