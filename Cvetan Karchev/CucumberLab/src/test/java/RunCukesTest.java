5741d5b454fe0ca63fd882a34cde5ec731623a50
Ceco <tsvetan.karchev@xoomworks.com>
2016-06-26 19:45:18 +0300

    update cucumberlab

--- Cvetan Karchev/CucumberLab/src/test/java/RunCukesTest.java
+++ Cvetan Karchev/CucumberLab/src/test/java/RunCukesTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(strict = true,
     features = {"classpath:features/Logout.feature"},
-    format = {"pretty","html:target/cucumber"},
+    format = {"pretty","json:target/Logout.json"},
     tags = {"@complete"})
 
 public class RunCukesTest {
